4b11959b37ffe045645c125ceb8ccbd6b84f1869
Brendan Tackney <brendan_tackney@daytonastate.edu>
2017-11-11 12:48:00 -0500

    original v2

--- api/server.js
+++ api/server.js
@@ -3,7 +3,7 @@ var http = require('http').Server(app);
 var io = require('socket.io')(http);
 
 io.on('connection', function(socket){
-    console.log('a user connected');
+    console.log('a user connected' + socket.id);
 });
 
 http.listen(3000, function(){
