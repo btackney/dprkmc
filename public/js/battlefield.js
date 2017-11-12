7b239121be2ba442406a38fa2ebcd490c3eb09e6
Brendan Tackney <brendan_tackney@daytonastate.edu>
2017-11-12 09:23:05 -0500

    up & running

--- public/js/battlefield.js
+++ public/js/battlefield.js
@@ -68,7 +68,7 @@ var battlefield = {
         for(var prop in cities){
             console.log(cities[prop].flag);
             var bunny = PIXI.Sprite.fromImage(cities[prop].flag);
-            cities[prop].y = battlefield.battlefieldHeight -107;
+            cities[prop].y = battlefield.battlefieldHeight - 107;
             bunny.x = cities[prop].x;
             bunny.y = cities[prop].y;
             cities[prop].bunnyRef = bunny;
@@ -76,8 +76,20 @@ var battlefield = {
         }
     },
     addRocket: function(sx,sy,destx,desty) {
-        var dx=(destx-sx)/100;
-        var dy=(desty-sy)/100;
+
+        var mx = (destx-sx);
+        var my = (desty-sy);
+        //var dist = Math.sqrt(mx * mx + my * my);
+        var angle = (( (Math.atan2( my, mx ) * 180 )  )/ Math.PI) ;//+ (90/Math.PI);
+        angle=angle+90;
+
+        var dx=(destx-sx)/50;
+        var dy=(desty-sy)/50;
+
+        angle=angle * (Math.PI/180);
+
+        var aa=angle;
+        aa=aa*(180/Math.PI)-90;
         var bunny = PIXI.Sprite.fromImage('img/rocket.png');
         bunny.anchor.set(0.5);
         bunny.x = sx;
@@ -86,7 +98,8 @@ var battlefield = {
         bunny.destY=desty;
         bunny.dx = dx;
         bunny.dy = dy;
-        bunny.rotation = 0.0;
+        bunny.life=Date.now();
+        bunny.rotation = angle;
         battlefield.rockets.push(bunny);
         app.stage.addChild(bunny);
     },
@@ -99,30 +112,73 @@ var battlefield = {
         bunny.destY= 1080;
         bunny.dx = (bunny.destX-bunny.x)/(500+Math.random()*250);
         bunny.dy = (bunny.destY-bunny.y)/(500+Math.random()*250);
+        bunny.life=Date.now();
         battlefield.missiles.push(bunny);
         app.stage.addChild(bunny);
     },
-    destroyMissles: function(x,y){
+    destroyMissles: function(x,y,obj2){
         for (var i=0;i<battlefield.missiles.length;i++){
             var obj = battlefield.missiles[i];
-            if(distance(x, y, obj.x,obj.y) < 5){
-                obj.y = battlefield.battlefieldHeight;
+            if(distance(x, y, obj.x,obj.y) < 8){
+                obj.y = battlefield.battlefieldHeight+100;
+                obj.life=0;
+                obj2.y=-1000;
             }
         }
     },
     explosion: function(x,y){
 
     },
+    removeCrossHair: function(){
+      for(var i =0;i< battlefield.hairs.length;i++){
+          var obj = battlefield.hairs[i];
+          obj.x = battlefield.battlefieldWidth+3000;
+      }
+    },
+    removeDeadFromStageM: function() {
+        var missileLifeSpan=15000;
+        var nn=Date.now();
+        for (var i=battlefield.missiles.length-1;i>=0 ; i--){
+            var obj=battlefield.missiles[i];
+            if (nn-obj.life>missileLifeSpan) {
+                //console.log("dead " );
+                for (var j = app.stage.children.length - 1; j >= 0; j--) {
+                    if (app.stage.children[j].life) {
+                        if ((nn - app.stage.children[j].life) > missileLifeSpan) {
+                            app.stage.removeChild(app.stage.children[j]);
+                        }
+                    }
+                };
+                battlefield.missiles.splice(i,1);
+            }
+        }
+    },
+
+    removeDeadFromStage: function() {
+        var rocketlifespan=22222;
+        var nn=Date.now();
+        for (var i=battlefield.rockets.length-1;i>=0 ; i--){
+            var obj=battlefield.rockets[i];
+            if (nn-obj.life>rocketlifespan) {
+                //console.log("dead " );
+                for (var j = app.stage.children.length - 1; j >= 0; j--) {
+                    if (app.stage.children[j].life) {
+                        if ((nn - app.stage.children[j].life) > rocketlifespan) {
+                            app.stage.removeChild(app.stage.children[j]);
+                        }
+                    }
+                };
+                battlefield.rockets.splice(i,1);
+            }
+        }
+    },
+
     moveRockets: function() {
         for (var i=0;i<battlefield.rockets.length;i++){
             var obj=battlefield.rockets[i];
             obj.x+=obj.dx;
             obj.y+=obj.dy;
-            if (distance(obj.x,obj.y,obj.destX,obj.destY)<5) {
-                battlefield.destroyMissles(obj.x, obj.y);
-                obj.y=-100;
-                console.log("boom"); // todo actually remove items from stage
-            }
+            battlefield.destroyMissles(obj.x, obj.y,obj);
         }
     },
     moveMissiles: function() {
@@ -151,10 +207,13 @@ var battlefield = {
     },
     startGame: function() {
         setInterval(function(){ battlefield.addMissile() }, 1000);
+        setInterval(function(){ battlefield.removeCrossHair() }, 5000);
         // Listen for animate update
         app.ticker.add(function(delta) {
             battlefield.moveRockets();
             battlefield.moveMissiles();
+            battlefield.removeDeadFromStage();
+            battlefield.removeDeadFromStageM();
         });
     }
 };
@@ -184,7 +243,7 @@ $( document ).ready(function() {
 
         socket.on('joined', function(socketid){
             console.log('were u at' + socketid);
-            battlefield.addCrosshair(socketid,500,400);
+            battlefield.addCrosshair(socketid,-999,-999);
         });
         socket.on('disconnect', function (data) {
             battlefield.viewerSocket = null;
@@ -207,18 +266,28 @@ $( document ).ready(function() {
         socket.on('battlefield_launch_rocket', function(data){
             var px = data.data.x / data.data.w;
             var py = data.data.y / data.data.h;
-            battlefield.addRocket(battlefield.battlefieldWidth/2, battlefield.battlefieldHeight-50, px*battlefield.battlefieldWidth, py*battlefield.battlefieldHeight);
+            var startx = battlefield.battlefieldWidth/2;
+            for(var prop in cities){
+                console.log("why wont u find me" + prop + " "+ data.data.city);
+                if(prop === data.data.city){
+                    startx = cities[prop].x + 90;
+                }
+            }
+            console.log("startx " + startx);
+            battlefield.addRocket(startx, battlefield.battlefieldHeight-50, px*battlefield.battlefieldWidth, py*battlefield.battlefieldHeight);
         });
         setInterval(function(){
             var missilePositions = [];
             for(var i=0;i< battlefield.missiles.length; i++){
-                var currentMissile = {
-                    x: battlefield.missiles[i].x,
-                    y: battlefield.missiles[i].y
+                if(battlefield.missiles[i].y > 0 && battlefield.missiles[i].y < 1080){
+                    var currentMissile = {
+                        x: battlefield.missiles[i].x,
+                        y: battlefield.missiles[i].y
+                    }
+                    missilePositions.push(currentMissile);
                 }
-                missilePositions.push(currentMissile);
             }
             socket.emit('missile_positions', missilePositions)
-        }, 2000);
+        }, 500);
     });
 });
--- public/js/weaponSystem.js
+++ public/js/weaponSystem.js
@@ -1,6 +1,42 @@
 var mySocketSaved=null;
 var globalMouseIsDown=false;
 
+var roundRobin=0;
+
+function isFullScreen() {
+    return (document.fullScreenElement && document.fullScreenElement !== null) || document.mozFullScreen || document.webkitIsFullScreen;
+}
+
+function requestFullScreen(element) {
+    if (element.requestFullscreen)
+        element.requestFullscreen();
+    else if (element.msRequestFullscreen)
+        element.msRequestFullscreen();
+    else if (element.mozRequestFullScreen)
+        element.mozRequestFullScreen();
+    else if (element.webkitRequestFullscreen)
+        element.webkitRequestFullscreen();
+}
+
+function exitFullScreen() {
+    if (document.exitFullscreen)
+        document.exitFullscreen();
+    else if (document.msExitFullscreen)
+        document.msExitFullscreen();
+    else if (document.mozCancelFullScreen)
+        document.mozCancelFullScreen();
+    else if (document.webkitExitFullscreen)
+        document.webkitExitFullscreen();
+}
+
+function toggleFullScreen(element) {
+    if (isFullScreen()) {
+        exitFullScreen();
+    } else {
+        requestFullScreen(element || document.documentElement);
+    }
+}
+
 function getParameterByName(name) {
     name = name.replace(/[\[]/, "\\\[").replace(/[\]]/, "\\\]");
     var regex = new RegExp("[\\?&]" + name + "=([^&#]*)"),
@@ -8,54 +44,75 @@ function getParameterByName(name) {
     return results === null ? "" : decodeURIComponent(results[1].replace(/\+/g, " "));
 }
 
+var weaponSystem = {
+    missilePositions: function(data){
+        console.log(data);
+    }
+}
+var clientWidth = window.innerWidth;
+var clientHeight = window.innerHeight;
 $(document).ready(function() {
-    var clientWidth = window.innerWidth;
-    var clientHeight = window.innerHeight;
+     clientWidth = window.innerWidth;
+     clientHeight = window.innerHeight;
+
     $(window).resize(function(){
         clientWidth = window.innerWidth;
         clientHeight = window.innerHeight;
     });
-    var clientInfo = {
-        name: Date.now()+Math.floor(Math.random()),
-        type: 'weaponSystem',
-        city: getParameterByName('city'),
-        game: getParameterByName('game')
-    };
+
     var socket = io.connect("https://dprkmc.com:8443");
     socket.on('connect', function(){
+        var clientInfo = {
+            name: Date.now()+Math.floor(Math.random()),
+            type: 'weaponSystem',
+            city: getParameterByName('city'),
+            game: getParameterByName('game')
+        };
         mySocketSaved=socket;
         socket.emit('identify', clientInfo);
         socket.on('battlefield_missile_positions', function(data){
-            console.log(JSON.stringify(data))
+            //console.log('battlefield missiles positions'+ data.length);
+            $('#display').empty();
+            for(var i=0;i<data.length;i++){
+                var d="<div style='position:absolute;left:{{left}};top:{{top}};background-color:red;color:blue;'>XXX</div>";
+                d=d.replace("{{left}}",( Math.floor(data[i].x)/1920)*clientWidth + "px");
+                d=d.replace("{{top}}",( Math.floor(data[i].y)/1080)*clientHeight + "px");
+                console.log(d);
+                $('#display').append(d);
+            }
+            //console.log(JSON.stringify(data))
         })
     });
-    // window.onmousedown = function(e){
-    //     globalMouseIsDown=true;
-    // };
-
-    // window.onmousemove = function(e) {
-    //     var obj = {};
-    //     obj.x = e.clientX;
-    //     obj.y = e.clientY;
-    //     if (globalMouseIsDown) {
-    //         if (mySocketSaved) {
-    //             console.log('emitted move crosshair ' + obj);
-    //             mySocketSaved.emit("move_crosshair", obj);
-    //         }
-    //     }
-    // };
-
-    // window.onmouseup = function(e){
-    //     globalMouseIsDown=false;
-    //     var obj = {};
-    //     obj.x = e.clientX;
-    //     obj.y = e.clientY;
-    //     obj.w = clientWidth;
-    //     obj.h = clientHeight;
-    //     if (mySocketSaved) {
-    //         mySocketSaved.emit("launch_rocket", obj);
-    //     }
-    // };
+    window.onmousedown = function(e){
+        globalMouseIsDown=true;
+    };
+    window.onmousemove = function(e) {
+        var obj = {};
+        obj.x = e.clientX;
+        obj.y = e.clientY;
+        obj.w = clientWidth;
+        obj.h = clientHeight;
+        obj.socketid=mySocketSaved.id;
+        if (globalMouseIsDown) {
+            if (mySocketSaved) {
+                console.log('emitted move crosshair ' + obj);
+                mySocketSaved.emit("move_crosshair", obj);
+            }
+        }
+    };
+    window.onmouseup = function(e){
+        globalMouseIsDown=false;
+        var obj = {};
+        obj.x = e.clientX;
+        obj.y = e.clientY;
+        obj.w = clientWidth;
+        obj.h = clientHeight;
+        obj.socketid=mySocketSaved.id;
+        obj.city = getParameterByName('city');
+        if (mySocketSaved) {
+            mySocketSaved.emit("launch_rocket", obj);
+        }
+    };
 
     window.ontouchstart = function(e) {
         globalMouseIsDown=true;
@@ -75,6 +132,9 @@ $(document).ready(function() {
         }
     };
     window.ontouchmove = function(e) {
+        roundRobin++;
+        if (roundRobin>5) roundRobin=0;
+
         var pageX =  e.targetTouches[0].pageX ;
         var pageY = e.targetTouches[0].pageY ;
         var obj = {};
@@ -86,7 +146,7 @@ $(document).ready(function() {
         if (globalMouseIsDown) {
             if (mySocketSaved) {
                 console.log('emitted move crosshair ' + obj);
-                mySocketSaved.emit("move_crosshair", obj);
+               if (roundRobin==0) mySocketSaved.emit("move_crosshair", obj);
             }
         }
     };
@@ -98,6 +158,7 @@ $(document).ready(function() {
         obj.y = pageY;
         obj.w = clientWidth;
         obj.h = clientHeight;
+        obj.city = getParameterByName('city');
         if (mySocketSaved) {
             mySocketSaved.emit("launch_rocket", obj);
         }
